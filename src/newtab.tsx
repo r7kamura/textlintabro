2f232881bf1ecc752c519781d717003754f0a9a2
r7kamura <r7kamura@gmail.com>
2024-01-02 00:39:11 +0900

    Run textlint onDidChangeModelContent

--- src/newtab.tsx
+++ src/newtab.tsx
@@ -1,12 +1,42 @@
 import './newtab.css'
-import * as monaco from "monaco-editor";
+import "monaco-editor/esm/vs/editor/editor.all.js";
+import "monaco-editor/esm/vs/basic-languages/markdown/markdown.contribution";
+import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
+import EditorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
+import TextlintWorker from "./textlint-worker?worker";
 
-const editorElement = document.querySelector<HTMLDivElement>("#editor")!;
-monaco.editor.create(editorElement, {
-  fontSize: 18,
-  language: "markdown",
-  lineHeight: 1.6,
-  minimap: { enabled: false },
-  padding: { bottom: 16, top: 16 },
-  theme: "vs-dark",
+self.MonacoEnvironment = {
+  getWorker() {
+    return new EditorWorker();
+  },
+};
+
+const editor = monaco.editor.create(
+  document.querySelector<HTMLDivElement>("#editor")!,
+  {
+    fontSize: 18,
+    language: "markdown",
+    lineHeight: 1.6,
+    minimap: { enabled: false },
+    padding: { bottom: 16, top: 16 },
+    theme: "vs-dark",
+  }
+);
+
+const textlintWorker = new TextlintWorker();
+
+textlintWorker.onmessage = function (event) {
+  if (event.data.command !== "lint:result") {
+    return;
+  }
+
+  console.log(event);
+};
+
+editor.onDidChangeModelContent(() => {
+  textlintWorker.postMessage({
+    command: "lint",
+    text: editor.getValue(),
+    ext: ".md",
+  });
 });
